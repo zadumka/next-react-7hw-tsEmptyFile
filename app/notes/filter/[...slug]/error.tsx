218a2433a695f83e4d59c0747d097dbcb9979111
zadumka <alenahas1987@gmail.com>
2025-05-22 14:08:04 +0300

    add empty file

--- app/notes/filter/[...slug]/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-type ErrorProps = {
-  error: Error;
-};
-
-export default function Error({ error }: ErrorProps) {
-  return <p>{`Could not fetch the list of notes. ${error.message}`}</p>;
-}
